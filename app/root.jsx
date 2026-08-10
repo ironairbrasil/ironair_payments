71e10076c1d516e10c8b426f4868b251766d9342
Ayrton Borges <ayrtonborges@Ayrtons-MacBook-Air.local>
2026-08-10 15:13:33 -0300

    Add Microsoft Clarity checkout analytics

--- app/root.jsx
+++ app/root.jsx
@@ -13,6 +13,15 @@ export default function App() {
         <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2" />
         <link rel="icon" type="image/png" href="/iron-air-favicon.png?v=2" />
         <link rel="apple-touch-icon" href="/iron-air-favicon.png?v=2" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window,document,"clarity","script","y0bdyld647");`,
+          }}
+        />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
         <link
           rel="stylesheet"
