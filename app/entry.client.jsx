f816e15f67a180b601db6cc9c4dc5880708c5d37
Ayrton Borges <ayrtonborges@Ayrtons-MacBook-Air.local>
2026-09-15 17:35:45 -0300

    perf: hydrate offer only on user interaction

--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -5,7 +5,7 @@ const isOffer =
 if (!isOffer) {
   import("./hydrate.client");
 } else {
-  const events = ["pointerdown", "keydown", "touchstart", "scroll"];
+  const events = ["pointerdown", "keydown", "touchstart"];
   let timer;
 
   const hydrate = () => {
